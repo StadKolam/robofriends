54fcb6c49ff7f9fdf060e031facce4d8dfc31e7e
StadKolam <szecsitisza.adam@gmail.com>
2019-11-05 13:08:18 +0100

    Updates

---
+++ precache-manifest.98c54afb0e149ecb60b1d127fd84b783.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80841449deb6099f977f6e386c51529b",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "9f52f0c4861e117477fc",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "35bdc464c8b6592b9312",
+    "url": "/robofriends/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9f52f0c4861e117477fc",
+    "url": "/robofriends/static/js/2.a09a1be0.chunk.js"
+  },
+  {
+    "revision": "35bdc464c8b6592b9312",
+    "url": "/robofriends/static/js/main.f2a5e4cf.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  }
+]);
